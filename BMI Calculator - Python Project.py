609a1aa356f865de28b9a1fcc1cb0a0c4973ff1f
Patricia Gitiri <122744492+GITIRI@users.noreply.github.com>
2023-04-27 13:58:11 +0300

    BMI Calculator - Python Project

--- BMI Calculator - Python Project.py
+++ BMI Calculator - Python Project.py
@@ -1,98 +1,91 @@
-#!/usr/bin/env python
-# coding: utf-8
+BMI CALCULATOR
 
-#  # BMI CALCULATOR
-#  
-# https://mercer-health.com/services/weight-management-center/bmi-calculator#:~:text=Body%20Mass%20Index%2C%20or%20BMI,inches%20x%20height%20in%20inches
-# 
-# 
-
-# In[1]:
-
-
-#create variables that accept inputs
-
-name = input("Enter your name: ")
-
-weight = int (input("Enter your weight in kilograms: "))
-
-height = float(input("Enter your height in metres: "))
-
-BMI = (weight) / (height * height)
-
-BMI = round (BMI, 1)
-
-print (BMI)
-print (name)
-
-
-# In[ ]:
-
-
+#importing the necessary libraries/modules
 from tkinter import *
 from tkinter import messagebox
 
+#define a function to get the name of the user & calculate BMI
+def calculate_BMI():
+    name = name_tf.get()
+    weight = int(weight_tf.get())
+    height = float(height_tf.get())
+    BMI = (weight) / (height * height)
+    BMI = round(BMI, 1)
+    BMI_index (name, BMI)
+    
+#define a function to reset the entries
 def reset_entry():
+    name_tf.delete(0,'end')
     age_tf.delete(0,'end')
     height_tf.delete(0,'end')
     weight_tf.delete(0,'end')
 
-def calculate_bmi():
-    kg = int(weight_tf.get())
-    m = int(height_tf.get())/100
-    bmi = kg/(m*m)
-    bmi = round(bmi, 1)
-    bmi_index(bmi)
-
-def bmi_index(bmi):
-    
-    if bmi < 18.5:
-        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Underweight')
-    elif (bmi > 18.5) and (bmi < 24.9):
-        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Normal')
-    elif (bmi > 24.9) and (bmi < 29.9):
-        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Overweight')
-    elif (bmi > 29.9):
-        messagebox.showinfo('bmi-pythonguides', f'BMI = {bmi} is Obesity') 
-    else:
-        messagebox.showerror('bmi-pythonguides', 'something went wrong!')   
-
-ws = Tk()
-ws.title('PythonGuides')
-ws.geometry('400x300')
-ws.config(bg='#686e70')
+#Display name, BMI & message based on user's details    
+def BMI_index(name, BMI):
+    if BMI > 0: 
+        if BMI < 18.5:
+             messagebox.showinfo('BMI info', f'{name}, your BMI is {BMI}, which means that you are underweight with minimal health risk')
+        elif BMI <= 24.9:
+            messagebox.showinfo('BMI info', f'{name}, your BMI is {BMI}, which means that you have normal weight with minimal health risk')
+        elif BMI <= 29.9:
+             messagebox.showinfo('BMI info', f'{name}, your BMI is {BMI},, which means that you are overweight with an increased health risk')
+        elif BMI <= 34.9:
+            messagebox.showinfo('BMI info', f'{name}, your BMI is {BMI}, which means that you are obese with a high health risk')
+        elif BMI <= 39.9:
+            messagebox.showinfo('BMI info', f'{name}, your BMI is {BMI}, which means that you are severely obese with a very high health risk')
+        else:
+            messagebox.showinfo('BMI info', f'{name}, your BMI is {BMI}, which means that you are morbidly obese with an extremely high health risk')
+            
+#create main window for the GUI            
+ws= Tk()
+ws.title('BMI Calculator')
+ws.geometry('300x250')
+ws.config(bg='#FFFFF0')
 
 var = IntVar()
 
+#create frame
 frame = Frame(
     ws,
-    padx=10, 
-    pady=10
+    padx=15, 
+    pady=15
 )
 frame.pack(expand=True)
+
+#create labels, entry widgets & radio buttons
+name_lb = Label(
+    frame,
+    text="Enter Your Name"
+)
+name_lb.grid(row=1, column=1)
+
+name_tf = Entry(
+    frame, 
+)
+name_tf.grid(row=1, column=2, pady=5)
 
 
 age_lb = Label(
     frame,
-    text="Enter Age (2 - 120)"
+    text="Enter Your Age (0 - 100)"
 )
-age_lb.grid(row=1, column=1)
+age_lb.grid(row=2, column=1)
 
 age_tf = Entry(
     frame, 
 )
-age_tf.grid(row=1, column=2, pady=5)
+age_tf.grid(row=2, column=2, pady=5)
 
 gen_lb = Label(
     frame,
     text='Select Gender'
 )
-gen_lb.grid(row=2, column=1)
+gen_lb.grid(row=3, column=1)
 
 frame2 = Frame(
     frame
 )
-frame2.grid(row=2, column=2, pady=5)
+frame2.grid(row=3, column=2, pady=5)
 
 male_rb = Radiobutton(
     frame2,
@@ -112,36 +105,37 @@ female_rb.pack(side=RIGHT)
 
 height_lb = Label(
     frame,
-    text="Enter Height (cm)  "
+    text="Enter Your Height (m)  "
 )
-height_lb.grid(row=3, column=1)
+height_lb.grid(row=4, column=1)
 
 weight_lb = Label(
     frame,
-    text="Enter Weight (kg)  ",
+    text="Enter Your Weight (kg)  ",
 
 )
-weight_lb.grid(row=4, column=1)
+weight_lb.grid(row=5, column=1)
 
 height_tf = Entry(
     frame,
 )
-height_tf.grid(row=3, column=2, pady=5)
+height_tf.grid(row=4, column=2, pady=5)
 
 weight_tf = Entry(
     frame,
 )
-weight_tf.grid(row=4, column=2, pady=5)
+weight_tf.grid(row=5, column=2, pady=5)
 
 frame3 = Frame(
     frame
 )
-frame3.grid(row=5, columnspan=3, pady=10)
+frame3.grid(row=6, columnspan=3, pady=10)
 
+#create calculate, reset & exit buttons
 cal_btn = Button(
     frame3,
     text='Calculate',
-    command=calculate_bmi
+    command = calculate_BMI
 )
 cal_btn.pack(side=LEFT)
 
@@ -159,28 +153,6 @@ exit_btn = Button(
 )
 exit_btn.pack(side=RIGHT)
 
-ws.mainloop()
-
-
-# In[3]:
-
-
-if BMI < 18.5:
-    print (name,", you are underweight with minimal health risk")
-elif BMI <= 24.9:
-    print (name,", you have normal weight with minimal health risk")
-elif BMI <= 29.9:
-    print (name,", you are overweight with an increased health risk")
-elif BMI <= 34.9:
-    print (name,", you are obese with a high health risk")
-elif BMI <= 39.9:
-    print (name,", you are severely obese with a very high health risk")
-else:
-    print (name,", you are morbidly obese with an extremely high health risk")
-
-
-# In[ ]:
-
-
-
-
+#run the window
+ws.mainloop()         
+    
